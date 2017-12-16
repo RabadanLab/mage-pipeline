1e82c267a9f40343eea8eb083d1eed2ddee3fb3e
Ubuntu <ubuntu@ip-172-31-19-125.us-east-2.compute.internal>
2017-12-16 11:24:45 +0000

    Add R requirements

---
+++ requirements.R
@@ -0,0 +1,3 @@
+list.of.packages <- c("tidyverse", "rstan")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
